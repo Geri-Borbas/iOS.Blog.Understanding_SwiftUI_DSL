14b723680ce2409a3e2f32d387df7b36bc4055fc
Geri Borbás <geri@eppz.eu>
2020-06-26 02:00:45 +0200

    Initial Commit

---
+++ SwiftUI_DSL/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  SwiftUI_DSL
+//
+//  Created by Geri Borbás on 2020. 06. 26..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI_DSL/SwiftUI_DSLApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_DSLApp.swift
+//  SwiftUI_DSL
+//
+//  Created by Geri Borbás on 2020. 06. 26..
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_DSLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
